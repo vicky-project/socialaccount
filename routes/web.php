a1c70a7becb2778b344dbc0a4c443f28e361fdd0
vicky <pratamavr@gmail.com>
2026-03-08 01:39:28 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\SocialAccount\Http\Controllers\SocialLoginController;
+use Modules\SocialAccount\Http\Controllers\ProfileController;
 
 Route::group(['middleware' => 'web'], function() {
   Route::get('auth/{provider}', [SocialLoginController::class, 'redirectToProvider'])->name('social.login');
   Route::get('auth/{provider}/callback', [SocialLoginController::class, 'handleProviderCallback'])->name('social.callback');
 });
 
-Route::group(['middleware' => 'auth'], function() {});
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/profile/social', [ProfileController::class, 'index'])->name('profile.social');
+  Route::get('/profile/social/{provider}', [ProfileController::class, 'connect'])->name('profile.social.connect');
+  Route::get('/profile/social/{id}', [ProfileController::class, 'disconnect'])->name('profile.social.disconnect');
+});
