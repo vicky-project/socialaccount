96ba3e8e5e5fb5e4eb435e5af275aba5b9b6fae8
vicky <pratamavr@gmail.com>
2026-03-08 00:10:38 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\SocialAccount\Http\Controllers\SocialAccountController;
+use Modules\SocialAccount\Http\Controllers\SocialLoginController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('socialaccounts', SocialAccountController::class)->names('socialaccount');
+Route::group(['middleware' => 'web'], function() {
+  Route::get('auth/{provider}', [SocialLoginController::class, 'redirectToProvider'])->name('social.login');
+  Route::get('auth/{provider}/callback', [SocialLoginController::class, 'handleProviderCallback'])->name('social.callback');
 });
+
+Route::group(['middleware' => 'auth'], function() {});
