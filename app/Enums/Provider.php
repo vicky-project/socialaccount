71e51718bc00c839871fd499ca162d15ab6bd93f
vicky <pratamavr@gmail.com>
2026-03-08 01:34:41 +0800

    updates

--- app/Enums/Provider.php
+++ app/Enums/Provider.php
@@ -3,11 +3,13 @@ namespace Modules\SocialAccount\Enums;
 
 enum Provider: string
 {
+  case GOOGLE = "google";
   case TELEGRAM = "telegram";
 
     public function label(): string
     {
       return match ($this) {
+        self::GOOGLE => "Google",
         self::TELEGRAM => "Telegram",
       };
     }
