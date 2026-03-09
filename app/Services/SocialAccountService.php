18f8beb2bd605c394a9a826e828c2147380f4fd6
vicky <pratamavr@gmail.com>
2026-03-09 22:10:30 +0800

    updates

---
+++ app/Services/SocialAccountService.php
@@ -0,0 +1,45 @@
+<?php
+namespace Modules\SocialAccount\Services;
+
+use Modules\Users\Models\User;
+use Modules\SocialAccount\Models\SocialAccount;
+use Modules\SocialAccount\Interfaces\SocialAccountInterface;
+
+class SocialAccountService
+{
+  public function getByUserId(int $userId) {
+    return SocialAccount::whereHas("user", function ($query) use ($userId) {
+      $query->where("id", $userId);
+    })->get();
+  }
+
+  public function getByAuthlogId($authId) {
+    return SocialAccount::where("authlog_id", $authId)->get();
+  }
+
+  public function saveUserSocialAccountByProvider(
+    User $user,
+    SocialAccountInterface $model,
+    string $provider,
+    array $options = []
+  ) {
+    $authenticationLog = $user->latestAuthentication;
+
+    if ($authenticationLog) {
+      return $model->provider()->firstOrCreate(
+        [
+          "user_id" => $user->id,
+          "authlog_id" => $authenticationLog->id,
+        ],
+        [
+          "user_id" => $user->id,
+          "authlog_id" => $authenticationLog->id,
+          "provider" => $provider,
+          "provider_data" => $options ?? [],
+        ]
+      );
+    }
+
+    return $authenticationLog;
+  }
+}
